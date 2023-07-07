41f4a1278e913b4a32d961b4228e0b10f77b6a9f
pml204 <pml204@exeter.ac.uk>
2023-07-07 20:37:00 +0100

    Disable text print from synthetic dataset.

--- dpmm_fit.R
+++ dpmm_fit.R
@@ -1406,12 +1406,12 @@ runModel <- function(dataset, mcmc_iterations = 2500, L = 10, mcmc_chains = 2, s
   
   if (standardise == TRUE) {
     if (ncol(continuous) != 0) {
-      output <- list(dataset = synthpop::syn(dataset, k = 1)$syn, L = L, mcmc_chains = mcmc_chains, samples = samples, standardise = standardise, mean_values = mean_values, sd_values = sd_values)
+      output <- list(dataset = synthpop::syn(dataset, k = 1, print.flag = FALSE)$syn, L = L, mcmc_chains = mcmc_chains, samples = samples, standardise = standardise, mean_values = mean_values, sd_values = sd_values)
     } else {
-      output <- list(dataset = synthpop::syn(dataset, k = 1)$syn, L = L, mcmc_chains = mcmc_chains, samples = samples, standardise = standardise)
+      output <- list(dataset = synthpop::syn(dataset, k = 1, print.flag = FALSE)$syn, L = L, mcmc_chains = mcmc_chains, samples = samples, standardise = standardise)
     }
   } else {  
-    output <- list(dataset = synthpop::syn(dataset, k = 1)$syn, L = L, mcmc_chains = mcmc_chains, samples = samples, standardise = standardise)
+    output <- list(dataset = synthpop::syn(dataset, k = 1, print.flag = FALSE)$syn, L = L, mcmc_chains = mcmc_chains, samples = samples, standardise = standardise)
   }
   
   class(output) <- "dpmm_fit"
