61a497161a9dca9cd9958928a730b870ba0bb131
PM-Cardoso <p.cardoso01@hotmail.com>
2023-09-30 18:20:39 +0100

    First set up of the package.

---
+++ R/dataset_1.R
@@ -0,0 +1,12 @@
+#' Synthetic dataset 1
+#'
+#' A synthetic dataset comprised of two distinct clusters (1:500, 501:1000), where the values of both predictors do not overlap each other. One variable is informative of the other variable.
+#'
+#' @format ## `dataset_1`
+#' A data frame with 1,000 rows and 2 columns:
+#' \describe{
+#'   \item{X1}{Continuous variable, mean = 1, sd = 1; mean = 8, sd = 1}
+#'   \item{X2}{Continuous variable, mean = 2, sd = 1; mean = 9, sd = 1}
+#' }
+#' @source Synthetic
+"dataset_1"
---
+++ R/dataset_4.R
@@ -0,0 +1,15 @@
+#' Synthetic dataset 4
+#'
+#' A synthetic dataset, where Y is the outcome variable, X1/X2/X3 are continuous predictors and X4 is a categorical predictor.
+#'
+#' @format ## `dataset_4`
+#' A data frame with 500 rows and 5 columns:
+#' \describe{
+#'   \item{X1}{Continuous variable, mean = 8, sd = 3}
+#'   \item{X2}{Continuous variable, mean = 2, sd = 3}
+#'   \item{X3}{Continuous variable, mean = -1, sd = 3}
+#'   \item{X4}{Categorical variable, levels = 0 or 1}
+#'   \item{Y}{Continuous variable, response of regression}
+#' }
+#' @source Synthetic
+"dataset_4"
